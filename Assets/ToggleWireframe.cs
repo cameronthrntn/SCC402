d5d7611ac0a0a927bb18248645d4afb669176a73
TomBayley1 <thomas.bayley1@gmail.com>
2018-11-09 15:30:11 +0000

    Outlines, Extended Tracking, Wireframe, Hotspot Labels

---
+++ Assets/ToggleWireframe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ToggleWireframe : MonoBehaviour
+{
+
+	public GameObject monumentFull;
+	public GameObject monumentWireframe;
+	
+	public void setModelType()
+	{
+		bool isMonumentFullActive = monumentFull.activeSelf;
+		
+		monumentFull.SetActive(!isMonumentFullActive);
+		monumentWireframe.SetActive(isMonumentFullActive);
+	}
+}

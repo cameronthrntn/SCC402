b3a04e74b63e0b9a7a66920053dc47fef1871156
Tom Bayley <thomas.bayley1@gmail.com>
2018-11-12 18:14:15 +0000

    Make assistant smoothly move around when moving camera

--- Assets/AssistantMovement.cs
+++ Assets/AssistantMovement.cs
@@ -1,49 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AssistantMovement : MonoBehaviour
 {
 
-	private float assistantDistanceFromCamera = 20;
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
 
+	void Update()
+	{
+		// Setting the position manually like this will not work well with different phones screen sizes
+		// If all devices are 16:9 then it should be ok
+		// Example: Vector3(8, 3, 20) =
+		//		8 game metres to right of center screen
+		//		3 game metres above center of screen
+		//		20 game metres in front of camera
+		Vector3 targetPosition = Camera.main.transform.TransformPoint(new Vector3(8, 3, 20));
 
-	private float smoothMoveSpeed = 1;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-
-		Debug.LogError(pos.x + "  " + transform.position.x);
-
-		float distX = pos.x - transform.position.x;
-		float distY = pos.y - transform.position.y;
-
-		if (distX > smoothMoveSpeed)
-		{
-			distX = smoothMoveSpeed;
-		} else if (distX < -smoothMoveSpeed)
-		{
-			distX = -smoothMoveSpeed;
-		}
-
-		if (distY > smoothMoveSpeed)
-		{
-			distY = smoothMoveSpeed;
-		} else if (distY < -smoothMoveSpeed)
-		{
-			distY = -smoothMoveSpeed;
-		}
-
-		pos.x = Mathf.Clamp01(transform.position.x + distX);
-		pos.y = Mathf.Clamp01(transform.position.y + distY);
-		pos.z = assistantDistanceFromCamera;
-		transform.position = Camera.main.ViewportToWorldPoint(pos);
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 	}
 }

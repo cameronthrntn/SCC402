d4e7a729d63a01245d36e31f0b421056374eb68a
dominicwild <dominic55555@hotmail.com>
2018-11-16 02:58:20 +0000

    Smooth Picture Appearance
    Made the appearance of the picture smoother.

--- Assets/PictureDisplay.cs
+++ Assets/PictureDisplay.cs
@@ -5,30 +5,71 @@ using UnityEngine.UI;
 
 public class PictureDisplay : MonoBehaviour {
 
-    public GameObject hotImage;
-    public GameObject ARCamera;
-    public Button btn; 
+    public GameObject hotImage;         //The image GameObject to manipulate.
+    public GameObject ARCamera;         //The AR's camera.
+    public Button btn;                  //The button that triggers actions on the camera. It's probably on a hot spot.
+
+    
+    public float objectScale = 1.0f;
+    private Vector3 initialScale;
+    public float rateOfGrowth = 0.1f;      //Rate at which the image grows and shrinks as a percentage.
+    private float growth = 0f;              //The current growth
+    private bool growing = false;           //If the picture is growing or shrinking.
 
 
-	// Use this for initialization
-	void Start () {
-        hotImage.SetActive(false);
+    // Use this for initialization
+    void Start () {
+        //hotImage.SetActive(false);
 
         if (btn != null) {
             btn.onClick.AddListener(click);
         }
+
+        // record initial scale, use this as a basis
+        initialScale = transform.localScale;
+        hotImage.transform.localScale = initialScale*0f;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (ARCamera != null) {
-            //hotImage.transform.LookAt(ARCamera.transform);
             hotImage.transform.rotation = ARCamera.transform.rotation;
         }
-	}
+
+        lock (this) {
+            //Growth of picture
+            if (growing) {
+                if (growth >= 1) {
+                    growth = 1;
+                } else {
+                    growth += rateOfGrowth;
+                }
+            } else {
+                if (growth <= 0) {
+                    growth = 0;
+                } else {
+                    growth -= rateOfGrowth;
+                }
+            }
+        }
+
+        Plane plane = new Plane(ARCamera.transform.forward, ARCamera.transform.position);
+        float dist = plane.GetDistanceToPoint(hotImage.transform.position);
+        hotImage.transform.localScale = initialScale * dist * objectScale * growth;
+
+        GameObject.Find("Text").GetComponent<Text>().text = growth.ToString();
+    }
 
     private void click() {
-        //hotImage.GetComponent<MeshRenderer>().enabled = !hotImage.GetComponent<MeshRenderer>().enabled;
-        hotImage.SetActive(!hotImage.activeSelf);
+        //hotImage.SetActive(!hotImage.activeSelf);
+        lock (this) {
+            growing = !growing;
+            if (growing) { //Kick start growth process.
+                growth = rateOfGrowth;
+            } else {
+                growth = 1 - rateOfGrowth;
+            }
+        }
     }
 }

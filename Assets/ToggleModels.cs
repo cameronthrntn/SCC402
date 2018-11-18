d0c29c52c2ccf5ab6feb8aca222962d74e0e2514
TomBayley1 <thomas.bayley1@gmail.com>
2018-11-18 18:14:35 +0000

    Update castlePartial model. Make toggle models script work by finding models by name

--- Assets/ToggleModels.cs
+++ Assets/ToggleModels.cs
@@ -3,12 +3,36 @@
 public class ToggleModels : MonoBehaviour
 {
 
-	public GameObject monumentFull;
-	public GameObject monumentPartial;
-	public GameObject monumentDestroyed;
-	public GameObject monumentWireframe;
-	public GameObject monumentWireframePartial;
-	
+	private GameObject monumentFull;
+	private GameObject monumentPartial;
+	private GameObject monumentDestroyed;
+	private GameObject monumentWireframe;
+	private GameObject monumentWireframePartial;
+
+	public void Start()
+	{
+		GameObject castleModels = GameObject.Find("CastleModels");
+		GameObject castleWireframes = GameObject.Find("CastleWireframes");
+
+		monumentFull = FindObject(castleModels, "castleFull");
+		monumentPartial = FindObject(castleModels, "castlePartial");
+		monumentDestroyed = FindObject(castleModels, "castleDestroyed");
+
+		monumentWireframe = FindObject(castleWireframes, "wireframeFull");
+		monumentWireframePartial = FindObject(castleWireframes, "wireframePartial");
+	}
+
+	public static GameObject FindObject(GameObject parent, string name)
+	{
+		Transform[] trs= parent.GetComponentsInChildren<Transform>(true);
+		foreach(Transform t in trs){
+			if(t.name == name){
+				return t.gameObject;
+			}
+		}
+		return null;
+	}
+
 	public void toggleWireframe()
 	{
 

9c4ec8e73e9fcf9c339b67466b0b76d1dafe88bc
TomBayley1 <thomas.bayley1@gmail.com>
2018-12-04 17:07:47 +0000

    Revert "_"
    This reverts commit 1096ab22814300d52437556a555d521fcb7127cd.

--- Assets/Scripts/MediaDisplay.cs
+++ Assets/Scripts/MediaDisplay.cs
@@ -17,7 +17,8 @@ public class MediaDisplay : MonoBehaviour
     public float reductionMult = 0.02f;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         ARCamera = Camera.main.gameObject;
 
         // record initial scale, use this as a basis
@@ -32,7 +33,8 @@ public class MediaDisplay : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (ARCamera == null)
         {
             return;
